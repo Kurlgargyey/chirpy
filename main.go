40343c53fa1d8f053c8da6ed572a6cb6e3494e34
Kurlgargyey <nahsirkivar@gmail.com>
2024-11-22 00:13:26 +0100

    health check handler

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 
 func main() {
 	srvMux := http.NewServeMux()
-	srvMux.Handle("/", http.FileServer(http.Dir(".")))
+	srvMux.Handle("/app/",
+		http.StripPrefix("/app",
+			http.FileServer(http.Dir("."))))
+	srvMux.HandleFunc("/healthz",
+		func(response http.ResponseWriter, req *http.Request) {
+			response.Header().Add("Content-Type", "text/plain; charset=utf-8")
+			response.Write([]byte("OK"))
+		})
 	server := http.Server{
 		Handler: srvMux,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
 	server.ListenAndServe()
 }
